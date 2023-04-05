1baeba86f72fffc27760dec7dda3b51cd8f4e274
Emilio <emilio_casado_de_galdo@hotmail.com>
2023-04-05 13:14:26 +0100

    Login done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,34 +1,41 @@
 <template>
   <v-container class="input">
+    Acceso a Takarabako
     <v-form fast-fail @submit.prevent>
-      <v-text-field  v-model="email" label="email" :rules="emailRules">
+      <v-text-field v-model="email" label="email" autofocus :rules="emailRules">
       </v-text-field>
-      <v-text-field 
-        type="password"
+      <v-text-field
+        :type="show1 ? 'text' : 'password'"
+        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="show1 = !show1"
         v-model="password"
+        counter
         label="password"
       ></v-text-field>
-      <v-container>
-        <v-row justify="space-around">
-          <v-spacer></v-spacer>
-          <v-col>
-            <v-btn @click="userLogin" type="submit" class="button" rounded="xs"
-              >Login
-              <span class="material-icons" id="iconCross">close</span>
-            </v-btn>
-          </v-col>
-          <v-col>
-            <v-btn class="button" rounded="xs" @click="goBack">
-              Volver
-              <span class="material-icons" id="iconCircle"
-                >radio_button_unchecked</span
-              >
-            </v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-        </v-row>
-      </v-container>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="userLogin" type="submit" class="button" rounded="xs"
+          >Login
+          <span class="material-icons" id="iconCross">close</span>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="button" rounded="xs" @click="goBack">
+          Volver
+          <span class="material-icons" id="iconCircle"
+            >radio_button_unchecked</span
+          >
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
     </v-form>
+  </v-container>
+  <v-container>
+    <v-row justify="end">
+      <v-col 
+      cols="3">
+        <RouterLink to="/signup">Quiero registrarme</RouterLink>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -50,6 +57,7 @@ export default {
     password: "",
     username: "",
     authStore: useAuthStore(),
+    show1: false,
   }),
   methods: {
     async userLogin() {
@@ -80,7 +88,6 @@ export default {
 .button {
   background-color: #3e5161;
   color: #a1acb4;
-  position: static;
 }
 
 #iconCross {
